0ecaeffa865e279894433bc1ab5c70622ad2c905
Aqib <aqibkazi97@gmail.com>
2017-02-02 18:29:50 +0530

    My Commit

---
+++ ecomm_backend/src/main/java/com/niit/backend/config/SpringSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.niit.backend.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource dataSource;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/admin/**")
+					.access("hasAuthority('ADMIN')")
+				.antMatchers("/user/**")
+					.access("hasAuthority('USER')")
+				.and()
+					.formLogin()
+						.loginPage("/login")
+							.defaultSuccessUrl("/")
+								.failureUrl("/login?error")
+								.usernameParameter("email")
+									.passwordParameter("password")
+						.and()
+							.logout()
+								.logoutSuccessUrl("/login?logout")
+									.invalidateHttpSession(true)
+						.and()
+							.exceptionHandling()
+								.accessDeniedPage("/403")
+						.and()
+							.csrf()
+								.disable();
+	}
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		String query = "SELECT USER_EMAIL, USER_ROLE FROM USERS WHERE USER_EMAIL = ?";
+		auth.jdbcAuthentication().dataSource(dataSource).authoritiesByUsernameQuery(query)
+				.usersByUsernameQuery("SELECT USER_EMAIL, USER_PASSWORD, USER_ENABLED FROM USERS WHERE USER_EMAIL = ?");
+	
+		
+	/*auth.inMemoryAuthentication().withUser("ansarifaisal480@gmail.com").password("test").roles("USER");*/
+	}
+}
+
